2ce8e09768235fc30c129890b3df6397235869bf
marcin10lw <marcin10lw@wp.pl>
2023-02-09 22:25:29 +0100

    created Main

---
+++ src/common/Main/index.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const Main = styled.main`
+  color: ${({ theme }) => theme.colors.staleGrey};
+`;
--- src/features/personalHomepage/PersonalHomepage.js
+++ src/features/personalHomepage/PersonalHomepage.js
@@ -1,12 +1,20 @@
 import { Container } from "../../common/Container";
 import ThemeSwitcher from "./ThemeSwitcher";
 import Header from "./Header";
+import { Main } from "../../common/Main";
+import Section from "../../common/Section";
+import { skillset } from "./skillset";
+import { toLearn } from "./toLearn";
 
 function PersonalHomepage() {
   return (
     <Container>
       <ThemeSwitcher />
       <Header />
+      <Main>
+        <Section heading="My skillset includes" dataList={skillset} />
+        <Section heading="What I want to learn next" dataList={toLearn} />
+      </Main>
     </Container>
   );
 }
