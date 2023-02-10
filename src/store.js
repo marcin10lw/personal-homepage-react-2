108b6f083b9d31240d13cdd8c1878ea8fe374ba0
marcin10lw <marcin10lw@wp.pl>
2023-02-10 12:06:07 +0100

    added projectsSlice to store

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import themeReducer from "./features/personalHomepage/themeSlice";
+import projectsReducer from "./features/personalHomepage/projectsSlice";
 import createSagaMiddleware from "@redux-saga/core";
 import rootSaga from "./rootSaga";
 
@@ -8,6 +9,7 @@ const sagaMiddleware = createSagaMiddleware();
 export const store = configureStore({
   reducer: {
     theme: themeReducer,
+    projects: projectsReducer,
   },
   middleware: [sagaMiddleware],
 });
