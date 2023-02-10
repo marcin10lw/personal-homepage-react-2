e03958a242c7e81edf74f0cfd42fb8b4a2d7737f
marcin10lw <marcin10lw@wp.pl>
2023-02-10 07:52:52 +0100

    added colors to theme.js

--- src/theme.js
+++ src/theme.js
@@ -7,6 +7,10 @@ export const theme = {
     scienceBlue: "#0366D6",
     dodgerBlue: "#2188FF",
     lightGrey: "#E5E5E5",
+    anakiwa: "#8CC2FF",
+    shipCove: "#6D93BE",
+    doveGrey: "#6D6D6D",
+    iron: "#D1D5DA",
   },
   breakpoints: {
     mobile: 767,
