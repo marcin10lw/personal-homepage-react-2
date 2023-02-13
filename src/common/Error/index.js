6c87f39806eb06ceb0371ded710bbfd9aa183f48
marcin10lw <marcin10lw@wp.pl>
2023-02-13 19:54:14 +0100

    created and styled Error component

---
+++ src/common/Error/index.js
@@ -0,0 +1,31 @@
+import { useSelector } from "react-redux";
+import { selectDarkTheme } from "../../features/personalHomepage/themeSlice";
+import { Button } from "../Button";
+import { StyledError, ErrorHeader, ErrorIcon, ErrorMessage } from "./styled";
+
+const Error = () => {
+  const darkTheme = useSelector(selectDarkTheme);
+
+  return (
+    <StyledError darkTheme={darkTheme}>
+      <ErrorIcon />
+      <ErrorHeader>Ooops! Something went wrong...</ErrorHeader>
+      <ErrorMessage>
+        Sorry, failed to load Github projects. You can check them directly on
+        Github.
+      </ErrorMessage>
+      <Button
+        darkTheme={darkTheme}
+        inError
+        as="a"
+        href="https://github.com/marcin10lw"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Go to Github
+      </Button>
+    </StyledError>
+  );
+};
+
+export default Error;
---
+++ src/common/Error/styled.js
@@ -0,0 +1,54 @@
+import styled, { css } from "styled-components";
+import { ReactComponent as DangerIcon } from "./images/danger-icon.svg";
+
+const bpMobile = ({ theme }) => theme.breakpoints.mobile;
+
+export const StyledError = styled.div`
+  text-align: center;
+  color: ${({ theme }) => theme.colors.mineShaft};
+  max-width: 420px;
+  margin: 88px auto 0;
+
+  @media (max-width: ${bpMobile}px) {
+    margin-top: 50px;
+  }
+
+  ${({ darkTheme }) =>
+    darkTheme &&
+    css`
+      color: ${({ theme }) => theme.colors.white};
+    `}
+`;
+
+export const ErrorIcon = styled(DangerIcon)`
+  width: 37px;
+
+  @media (max-width: ${bpMobile}px) {
+    width: 30px;
+  }
+`;
+
+export const ErrorHeader = styled.h3`
+  font-size: 24px;
+  font-weight: 700;
+  line-height: 29px;
+  letter-spacing: 0.05em;
+  margin: 16px 0 0 0;
+
+  @media (max-width: ${bpMobile}px) {
+    font-size: 18px;
+    margin: 10px 0 0 0;
+  }
+`;
+
+export const ErrorMessage = styled.p`
+  font-size: 20px;
+  line-height: 1.4;
+  letter-spacing: 0.05em;
+  margin: 32px 0;
+
+  @media (max-width: ${bpMobile}px) {
+    font-size: 16px;
+    margin: 20px 0;
+  }
+`;
