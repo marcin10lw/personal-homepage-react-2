cb2d90e6cf77837b05350f037b7c19d91d64654e
marcin10lw <marcin10lw@wp.pl>
2023-02-13 20:22:06 +0100

    gave Loading a darkTheme prop

--- src/common/Loading/index.js
+++ src/common/Loading/index.js
@@ -1,9 +1,15 @@
+import { useSelector } from "react-redux";
+import { selectDarkTheme } from "../../features/personalHomepage/themeSlice";
 import { StyledLoading, LoadingMessage, LoadingSpinner } from "./styled";
 
 const Loading = () => {
+  const darkTheme = useSelector(selectDarkTheme);
+
   return (
     <StyledLoading>
-      <LoadingMessage>Please wait, projects are being loaded...</LoadingMessage>
+      <LoadingMessage darkTheme={darkTheme}>
+        Please wait, projects are being loaded...
+      </LoadingMessage>
       <LoadingSpinner />
     </StyledLoading>
   );
--- src/common/Loading/styled.js
+++ src/common/Loading/styled.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const StyledLoading = styled.div`
   text-align: center;
@@ -9,6 +9,12 @@ export const LoadingMessage = styled.p`
   font-size: 20px;
   letter-spacing: 0.05em;
   color: ${({ theme }) => theme.colors.mineShaft};
+
+  ${({ darkTheme }) =>
+    darkTheme &&
+    css`
+      color: ${({ theme }) => theme.colors.white};
+    `}
 `;
 
 export const LoadingSpinner = styled.div`
