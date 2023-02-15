1cb56a75ac1ef31002de856b205eb2b77a238f98
marcin10lw <marcin10lw@wp.pl>
2023-02-15 19:31:34 +0100

    styled webkit scrollbar

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -9,6 +9,31 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: inherit;
   }
 
+  html::-webkit-scrollbar {
+    width: 0.7vw;
+  }
+  
+  html::-webkit-scrollbar-thumb {
+    background-color: #25252580;
+    border-radius: 4px;
+    
+    &:hover {
+      background-color: #252525bd;
+    }    
+  }
+  
+  html::-webkit-scrollbar-track {
+    background-color: #cfcfcf;
+    margin-block: 0.3em;
+    border-radius: 4px;
+    
+    ${({ darkTheme }) =>
+      darkTheme &&
+      css`
+        background-color: #8a8a8a;
+      `}
+  }
+
   body {
     font-family: 'Inter', sans-serif;
     background-color: ${({ theme }) => theme.colors.whiteLilac};
