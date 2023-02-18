7e769e3d233a073f24ba7ba5d0efe9f4ed068efa
marcin10lw <marcin10lw@wp.pl>
2023-02-18 17:59:54 +0100

    gave fetching projects delay

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,7 +1,7 @@
 import { all } from "redux-saga/effects";
 import { watchFetchProjects } from "./features/personalHomepage/projectsSaga";
-import { watchSaveDarkThemeToLocalStorage } from "./features/personalHomepage/themeSaga";
+import { watchSaveisDarkThemeToLocalStorage } from "./features/personalHomepage/themeSaga";
 
 export default function* rootSaga() {
-  yield all([watchSaveDarkThemeToLocalStorage(), watchFetchProjects()]);
+  yield all([watchSaveisDarkThemeToLocalStorage(), watchFetchProjects()]);
 }
