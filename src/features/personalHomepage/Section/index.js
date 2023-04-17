46269c1997f79b751694fbcdb1d7f9edd575ee24
marcin10lw <marcin10lw@wp.pl>
2023-04-17 23:52:19 +0200

    animated Section

--- src/features/personalHomepage/Section/index.js
+++ src/features/personalHomepage/Section/index.js
@@ -1,9 +1,20 @@
 import { StyledSection, SectionHeader, SectionList } from "./styled";
 import { ListItem } from "./ListItem";
+import { useInView } from "framer-motion";
+import { useRef } from "react";
 
 const Section = ({ heading, dataList }) => {
+  const sectionRef = useRef(null);
+  const isInView = useInView(sectionRef, { once: true });
+
   return (
-    <StyledSection>
+    <StyledSection
+      ref={sectionRef}
+      style={{
+        opacity: isInView ? 1 : 0,
+        transition: "opacity 0.5s ease-in-out",
+      }}
+    >
       <SectionHeader>{heading}</SectionHeader>
       <SectionList>
         {dataList.map((item) => (
